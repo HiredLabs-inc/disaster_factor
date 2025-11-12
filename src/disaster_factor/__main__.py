abe01d195efe2996c7ec3d045e74643442713fc9
Jeff Stock <jantonstock@gmail.com>
2025-11-12 12:08:52 -0800

    wired core to cli

---
+++ src/disaster_factor/__main__.py
@@ -0,0 +1,7 @@
+from .cli import main
+
+# Module Execution Path
+# Runs when calling `python3 -m disaster_factor` in CLI
+# This is the "Main" entrypoint
+if __name__ == "__main__":
+    raise SystemExit(main())
